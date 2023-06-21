2de67aa39b71dad67f59614c20bd57333419e3e2
Артем Черных <acac1212@yandex.ru>
2023-06-21 11:21:25 +1000

    install bootstrap and react-bootstrap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,11 @@
+import { Container } from 'react-bootstrap';
+
 const App = () => (
-  <div>
-    Hello World!
-  </div>
+  <Container className="vh-100 vw-100 bg-light">
+    <div className="text-center">
+      Hello World!
+    </div>
+  </Container>
 );
 
 export default App;
